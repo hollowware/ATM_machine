d9f8026b373a80ff389c25934a428f9f63e56f04
Arthur <artur.muchliado@gmail.com>
2019-11-15 15:41:48 +0200

    ++

---
+++ src/lt/bank/atm/ATM.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.bank.atm;
+
+/**
+ *
+ * @author IX Hero
+ */
+public class ATM extends Options {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Options op = new Options();
+        op.getLogin();
+        
+    }
+    
+}
